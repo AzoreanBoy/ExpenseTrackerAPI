acd8f8037a7cc94a19c3881af0f3e9213b8ace79
AzoreanBoy <pedroregoferreira@gmail.com>
2025-09-08 18:30:56 +0100

    Update settings.py
    Adicicionadas Definições da REST Framework

--- expenseTracker/settings.py
+++ expenseTracker/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     
     # Local apps
     "expensetrackerAPI",
+    
+    # Rest Framework
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -127,3 +130,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Rest Framework settings
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
