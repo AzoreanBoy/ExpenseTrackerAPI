baf5efe57798e7bb693d7651c9cf71d6748474f3
AzoreanBoy <pedroregoferreira@gmail.com>
2025-09-26 00:31:20 +0100

    Registering Models in Admin

--- expensetrackerAPI/admin.py
+++ expensetrackerAPI/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
-# Register your models here.
+# Models
+from .models import *
+
+# Customizing admin titles
+admin.site.site_header = "Expense Tracker Admin"
+admin.site.site_title = "Expense Tracker Admin Portal"
+admin.site.index_title = "Welcome to the Expense Tracker Admin Portal"
+
+
+# Registering models
+
+
+# Registering Category with custom admin
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "type")
+    search_fields = ("name", "type")
+    list_filter = ("type",)
+    ordering = ("name",)
+
+
+# Registering SubCategory
+@admin.register(SubCategory)
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "category", "category_type_display")
+    search_fields = ("name", "category__name", )
+    list_filter = ("category",)
+    ordering = ("name", "category__name", )
+
+    def category_type_display(self, obj):
+        return obj.category.get_type_display()
+    
+    category_type_display.short_description = "Category Type"
